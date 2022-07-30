60ecb6ac5575673cb73c7f727e1dbe2b28606fe3
ellismcdougald <ellismcdougald@icloud.com>
2022-07-30 16:34:16 -0600

    Tests that MenuBar component renders within Homepage component

---
+++ src/pages/Homepage.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Homepage from "./Homepage";
+
+describe("Homepage component", () => {
+  it("should render MenuBar component", () => {
+    render(<Homepage />);
+    const HomepageElement = screen.getByTestId("Homepage");
+    const MenuBar = screen.getByTestId("MenuBar");
+
+    expect(HomepageElement).toContainElement(MenuBar);
+  });
+});
