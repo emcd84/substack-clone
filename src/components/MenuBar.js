b0869337105af7e8ed0897c2485e23b0adfe6f06
ellismcdougald <ellismcdougald@icloud.com>
2022-07-30 16:35:30 -0600

    Adds test id to MenuBar div

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,7 +3,7 @@ import styles from "../styles/MenuBar.module.css";
 
 export default function MenuBar() {
   return (
-    <div id={styles.container}>
+    <div id={styles.container} data-testid="MenuBar">
       <div id={styles.headerContainer}>
         <h1 id={styles.headerText}>My Newsletter</h1>
       </div>
