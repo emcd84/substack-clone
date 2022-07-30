b83f06422dd4546aae19eb01cfcfbbfa9443be1d
ellismcdougald <ellismcdougald@icloud.com>
2022-07-30 16:20:23 -0600

    Sets up styling for MenuBar component

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styles from "../styles/MenuBar.module.css";
 
 export default function MenuBar() {
   return (
-    <div>
-      <h1>My Newsletter</h1>
-      <button>Subscribe</button>
-      <button>Sign In</button>
+    <div id={styles.container}>
+      <div id={styles.headerContainer}>
+        <h1 id={styles.headerText}>My Newsletter</h1>
+      </div>
+      <div id={styles.buttonContainer}>
+        <button id={styles.subscribeButton}>Subscribe</button>
+        <button id={styles.signInButton}>Sign In</button>
+      </div>
     </div>
   );
 
