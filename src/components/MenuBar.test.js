cd1c55890782b6124a33e5afa9085a8f7e37bbaf
ellismcdougald <ellismcdougald@icloud.com>
2022-07-30 20:45:17 -0600

    Tests bottom menu bar

--- src/components/MenuBar.test.js
+++ src/components/MenuBar.test.js
@@ -3,9 +3,11 @@ import { render, screen } from "@testing-library/react";
 import MenuBar from "./MenuBar";
 
 describe("MenuBar component", () => {
-  it("renders heading with newsletter name", () => {
+  it("renders heading with newsletter name first ", () => {
     render(<MenuBar />);
-    expect(screen.getByRole("heading").textContent).toMatch("My Newsletter");
+    expect(screen.getAllByRole("heading")[0].textContent).toMatch(
+      "My Newsletter"
+    );
   });
 
   it("renders two buttons", () => {
@@ -21,5 +23,20 @@ describe("MenuBar component", () => {
   it("renders sign in button second", () => {
     render(<MenuBar />);
     expect(screen.getAllByRole("button")[1].textContent).toMatch("Sign In");
+  });
+
+  it("renders home heading second", () => {
+    render(<MenuBar />);
+    expect(screen.getAllByRole("heading")[1].textContent).toMatch("Home");
+  });
+
+  it("renders archive heading third", () => {
+    render(<MenuBar />);
+    expect(screen.getAllByRole("heading")[2].textContent).toMatch("Archive");
+  });
+
+  it("renders about heading fourth", () => {
+    render(<MenuBar />);
+    expect(screen.getAllByRole("heading")[3].textContent).toMatch("About");
   });
 });
