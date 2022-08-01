1685807ffaf1a7e62afdd28d6f1a63bbecc80647
ellismcdougald <ellismcdougald@icloud.com>
2022-07-31 18:55:50 -0600

    Adds Archivepage routing

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Archivepage from "./pages/Archivepage";
 
 export default function RouteSwitch() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/archive' element={<Archivepage />} />
       </Routes>
     </BrowserRouter>
   );
