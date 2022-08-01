4ea86230638ed107aec81f337c890e4f79dac7ac
ellismcdougald <ellismcdougald@icloud.com>
2022-07-31 19:00:50 -0600

    Sets up component with MenuBar

---
+++ src/pages/Aboutpage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MenuBar from "../components/MenuBar";
+
+export default function Aboutpage() {
+  return (
+    <div data-testid='Aboutpage'>
+      <MenuBar mode='about' />
+    </div>
+  );
+}
