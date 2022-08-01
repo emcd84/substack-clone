1a8d6a3bbf1e218c0ee9c9b80e7cd97949c59af7
ellismcdougald <ellismcdougald@icloud.com>
2022-08-01 15:47:08 -0600

    Places MenuBar in RouteSwitch

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MenuBar from "./components/MenuBar";
 import Homepage from "./pages/Homepage";
 import Archivepage from "./pages/Archivepage";
 import Aboutpage from "./pages/Aboutpage";
@@ -7,6 +8,7 @@ import Aboutpage from "./pages/Aboutpage";
 export default function RouteSwitch() {
   return (
     <BrowserRouter>
+      <MenuBar mode='about' />
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/archive' element={<Archivepage />} />
