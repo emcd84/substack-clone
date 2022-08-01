f4acd6ce4f064806c474e1d5b64ba2e598e513d2
ellismcdougald <ellismcdougald@icloud.com>
2022-08-01 16:34:50 -0600

    Implements NavLink styling

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/MenuBar.module.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function MenuBar(props) {
   // const subscribeOnClick = () => {
@@ -10,16 +10,8 @@ export default function MenuBar(props) {
   //   return null;
   // };
 
-  const getBottomTextDivClassName = (mode, div) => {
-    if (mode === div) {
-      return `${styles.bottomTextDiv} ${styles.selectedDiv}`;
-    } else {
-      return `${styles.bottomTextDiv}`;
-    }
-  };
-
-  const getBottomTextClassName = (mode, div) => {
-    if (mode === div) {
+  const generateNavLinkClassName = (isActive) => {
+    if (isActive.isActive) {
       return `${styles.bottomText} ${styles.selectedText}`;
     } else {
       return `${styles.bottomText}`;
@@ -38,27 +30,24 @@ export default function MenuBar(props) {
         </div>
       </div>
       <div id={styles.bottomContainer}>
-        <div className={getBottomTextDivClassName(props.mode, "home")}>
-          <Link className={getBottomTextClassName(props.mode, "home")} to='/'>
-            Home
-          </Link>
-        </div>
-        <div className={getBottomTextDivClassName(props.mode, "archive")}>
-          <Link
-            className={getBottomTextClassName(props.mode, "archive")}
-            to='/archive'
-          >
-            Archive
-          </Link>
-        </div>
-        <div className={getBottomTextDivClassName(props.mode, "about")}>
-          <Link
-            className={getBottomTextClassName(props.mode, "about")}
-            to='/about'
-          >
-            About
-          </Link>
-        </div>
+        <NavLink
+          className={(isActive) => generateNavLinkClassName(isActive)}
+          to='/'
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={(isActive) => generateNavLinkClassName(isActive)}
+          to='/archive'
+        >
+          Archive
+        </NavLink>
+        <NavLink
+          className={(isActive) => generateNavLinkClassName(isActive)}
+          to='/about'
+        >
+          About
+        </NavLink>
       </div>
     </div>
   );
