a3f897a62d2293dec3b956db59f981539acde9a9
ellismcdougald <ellismcdougald@icloud.com>
2022-08-01 15:45:34 -0600

    Renders MenuBar within BrowserRouter and appropriately tests Links

--- src/components/MenuBar.test.js
+++ src/components/MenuBar.test.js
@@ -1,42 +1,69 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import MenuBar from "./MenuBar";
 
 describe("MenuBar component", () => {
   it("renders heading with newsletter name first ", () => {
-    render(<MenuBar />);
-    expect(screen.getAllByRole("heading")[0].textContent).toMatch(
-      "My Newsletter"
+    render(
+      <BrowserRouter>
+        <MenuBar />
+      </BrowserRouter>
     );
+    expect(screen.getByRole("heading").textContent).toMatch("My Newsletter");
   });
 
   it("renders two buttons", () => {
-    render(<MenuBar />);
+    render(
+      <BrowserRouter>
+        <MenuBar />
+      </BrowserRouter>
+    );
     expect(screen.getAllByRole("button").length).toBe(2);
   });
 
   it("renders subscribe button first", () => {
-    render(<MenuBar />);
+    render(
+      <BrowserRouter>
+        <MenuBar />
+      </BrowserRouter>
+    );
     expect(screen.getAllByRole("button")[0].textContent).toMatch("Subscribe");
   });
 
   it("renders sign in button second", () => {
-    render(<MenuBar />);
+    render(
+      <BrowserRouter>
+        <MenuBar />
+      </BrowserRouter>
+    );
     expect(screen.getAllByRole("button")[1].textContent).toMatch("Sign In");
   });
 
-  it("renders home heading second", () => {
-    render(<MenuBar />);
-    expect(screen.getAllByRole("heading")[1].textContent).toMatch("Home");
+  it("renders home link first", () => {
+    render(
+      <BrowserRouter>
+        <MenuBar />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("link")[0].textContent).toMatch("Home");
   });
 
-  it("renders archive heading third", () => {
-    render(<MenuBar />);
-    expect(screen.getAllByRole("heading")[2].textContent).toMatch("Archive");
+  it("renders archive link second", () => {
+    render(
+      <BrowserRouter>
+        <MenuBar />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("link")[1].textContent).toMatch("Archive");
   });
 
-  it("renders about heading fourth", () => {
-    render(<MenuBar />);
-    expect(screen.getAllByRole("heading")[3].textContent).toMatch("About");
+  it("renders about link fourth", () => {
+    render(
+      <BrowserRouter>
+        <MenuBar />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("link")[2].textContent).toMatch("About");
   });
 });
