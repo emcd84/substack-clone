7a1d09450513aca254a04be7bdfed806f1a5ab47
ellismcdougald <ellismcdougald@icloud.com>
2022-08-01 15:46:41 -0600

    Removes MenuBar test

--- src/pages/Archivepage.test.js
+++ src/pages/Archivepage.test.js
@@ -3,12 +3,4 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Archivepage from "./Archivepage";
 
-describe("Archivepage component", () => {
-  it("should render MenuBar component", () => {
-    render(<Archivepage mode='archive' />);
-    const ArchivepageElement = screen.getByTestId("Archivepage");
-    const MenuBar = screen.getByTestId("MenuBar");
-
-    expect(ArchivepageElement).toContainElement(MenuBar);
-  });
-});
+describe("Archivepage component", () => {});
--- src/pages/Homepage.test.js
+++ src/pages/Homepage.test.js
@@ -3,12 +3,4 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Homepage from "./Homepage";
 
-describe("Homepage component", () => {
-  it("should render MenuBar component", () => {
-    render(<Homepage mode='home' />);
-    const HomepageElement = screen.getByTestId("Homepage");
-    const MenuBar = screen.getByTestId("MenuBar");
-
-    expect(HomepageElement).toContainElement(MenuBar);
-  });
-});
+describe("Homepage component", () => {});
