57321cd0828038b68bcef272e27375f08b1d63d9
ellismcdougald <ellismcdougald@icloud.com>
2022-08-01 16:35:14 -0600

    Sets up basic skeleton

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,6 @@
-import React from "react";
+import { React, useState } from "react";
 import MenuBar from "../components/MenuBar";
+import { useLocation } from "react-router-dom";
 
 export default function Homepage() {
   return <div data-testid='Homepage'></div>;
