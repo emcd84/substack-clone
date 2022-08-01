7e91e555dddfe7c0b02b5af0d0f42280f9e3ff19
ellismcdougald <ellismcdougald@icloud.com>
2022-07-31 18:56:58 -0600

    Sets up component with MenuBar

---
+++ src/pages/Archivepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MenuBar from "../components/MenuBar";
+
+export default function Archivepage() {
+  return (
+    <div data-testid='Archivepage'>
+      <MenuBar mode='archive' />
+    </div>
+  );
+}
