eeb0dcebf2469092c7d6dd0e324c72c5bb63c40d
ellismcdougald <ellismcdougald@icloud.com>
2022-08-02 10:24:16 -0600

    Modifies dummy data for top post preview

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,15 @@
 import { React } from "react";
+import styles from "../styles/Homepage.module.css";
+import TopPostPreview from "../components/TopPostPreview";
 
 export default function Homepage() {
-  return <div data-testid='Homepage'></div>;
+  return (
+    <div id={styles.container} data-testid='Homepage'>
+      <TopPostPreview
+        title="Dissecting Brad Lambert's Liiga troubles"
+        subtitle="Why hasn't Lambert enjoyed the draft year success that was expected from him?"
+        author='DRAFTLOOKHOCKEY'
+      />
+    </div>
+  );
 }
