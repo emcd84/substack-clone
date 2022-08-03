c90bef8008a8c9d788ca2b571d1b822d77dec674
ellismcdougald <ellismcdougald@icloud.com>
2022-08-03 17:58:01 -0600

    Renders PostPreview component

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import { React } from "react";
 import styles from "../styles/Homepage.module.css";
 import TopPostPreview from "../components/TopPostPreview";
+import PostPreview from "../components/PostPreview";
 
 export default function Homepage() {
   return (
@@ -9,6 +10,12 @@ export default function Homepage() {
         title="Dissecting Brad Lambert's Liiga troubles"
         subtitle="Why hasn't Lambert enjoyed the draft year success that was expected from him?"
         author='DRAFTLOOKHOCKEY'
+      />
+      <PostPreview
+        title='Miro Heiskanen and passive/aggressive forechecks'
+        subtitle="Tampa Bay limits Miro Heiskanen's transition abilities byp giving him more time to make a play, not less"
+        author='DRAFTLOOKHOCKEY'
+        date='2022-08-01'
       />
     </div>
   );
