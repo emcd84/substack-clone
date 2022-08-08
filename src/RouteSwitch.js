0c992ed96e16fcaec85df6827889b5c709dd27c6
ellismcdougald <ellismcdougald@icloud.com>
2022-08-08 17:13:01 -0600

    Pulls post data from firestore

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,16 +1,34 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MenuBar from "./components/MenuBar";
 import Homepage from "./pages/Homepage";
 import Archivepage from "./pages/Archivepage";
 import Aboutpage from "./pages/Aboutpage";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function RouteSwitch() {
+  const [posts, setPosts] = useState([]);
+
+  async function getPostData() {
+    const returnArray = [];
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+      returnArray.push(doc.data());
+    });
+
+    setPosts(returnArray);
+  }
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+
   return (
     <BrowserRouter>
       <MenuBar />
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage posts={posts} />} />
         <Route path='/archive' element={<Archivepage />} />
         <Route path='/about' element={<Aboutpage />} />
       </Routes>
