27e778783c3aa9eb353c935f0a16a6cd9912a99b
ellismcdougald <ellismcdougald@icloud.com>
2022-08-08 17:14:10 -0600

    Feeds correct post data to components

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,34 +3,11 @@ import styles from "../styles/Homepage.module.css";
 import TopPostPreview from "../components/TopPostPreview";
 import PostContainer from "../components/PostContainer";
 
-export default function Homepage() {
-  const postArray = [
-    {
-      id: "1",
-      title: "Miro Heiskanen and aggressive/passive forechecks",
-      subtitle:
-        "Tampa Bay limits Miro Heiskanen's transition abilities bpy giving him more time to make a play, not less",
-      author: "draftlookhockey",
-      date: "2022-08-01",
-    },
-    {
-      id: "2",
-      title: "2022 NHL Draft Notebook: Volume 1",
-      subtitle:
-        "Thoughts, clips, and areas to track for a variety of prospects",
-      author: "draftlookhockey",
-      date: "2022-01-10",
-    },
-  ];
-
+export default function Homepage(props) {
   return (
     <div id={styles.container} data-testid='Homepage'>
-      <TopPostPreview
-        title="Dissecting Brad Lambert's Liiga troubles"
-        subtitle="Why hasn't Lambert enjoyed the draft year success that was expected from him?"
-        author='DRAFTLOOKHOCKEY'
-      />
-      <PostContainer posts={postArray} />
+      <TopPostPreview post={props.posts.slice(0, 1)} />
+      <PostContainer posts={props.posts.slice(1)} />
     </div>
   );
 }
