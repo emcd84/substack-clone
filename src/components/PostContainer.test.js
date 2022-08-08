07913a63ac1c2d047e2dfe97a30e95ea33905ab5
ellismcdougald <ellismcdougald@icloud.com>
2022-08-08 12:50:36 -0600

    Tests Post Container component

---
+++ src/components/PostContainer.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import PostContainer from "./PostContainer";
+
+describe("PostContainer component", () => {
+  it("renders 'New' heading", () => {
+    render(<PostContainer posts={[]} />);
+    expect(screen.getByRole("heading").textContent).toMatch("New");
+  });
+
+  const postArray = [
+    {
+      title: "Miro Heiskanen and aggressive/passive forechecks",
+      subtitle:
+        "Tampa Bay limits Miro Heiskanen's transition abilities bpy giving him more time to make a play, not less",
+      author: "draftlookhockey",
+      date: "2022-08-01",
+    },
+    {
+      title: "2022 NHL Draft Notebook: Volume 1",
+      subtitle:
+        "Thoughts, clips, and areas to track for a variety of prospects",
+      author: "draftlookhockey",
+      date: "2022-01-10",
+    },
+  ];
+  postArray.forEach((post) => {
+    it(`renders post with title ${post.title}`, () => {
+      render(<PostContainer posts={postArray} />);
+      expect(screen.getByText(post.title)).toBeInTheDocument();
+    });
+  });
+});
