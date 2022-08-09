5f70580b49a584dd7ab07c7d8bcf99575e7b99ad
ellismcdougald <ellismcdougald@icloud.com>
2022-08-09 13:50:46 -0600

    Renders post container

--- src/pages/Archivepage.js
+++ src/pages/Archivepage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import PostContainer from "../components/PostContainer";
+import styles from "../styles/Archivepage.module.css";
 
-export default function Archivepage() {
-  return <div data-testid='Archivepage'></div>;
+export default function Archivepage(props) {
+  return (
+    <div id={styles.container}>
+      <PostContainer posts={props.posts} />
+    </div>
+  );
 }
