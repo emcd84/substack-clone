fc3217e3e71401f2e74808a0f779061e3aebec33
ellismcdougald <ellismcdougald@icloud.com>
2022-08-09 14:39:38 -0600

    Tests Aboutpage

--- src/pages/Aboutpage.test.js
+++ src/pages/Aboutpage.test.js
@@ -2,3 +2,18 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Aboutpage from "./Aboutpage";
+import { wait } from "@testing-library/user-event/dist/utils";
+
+describe("Aboutpage component", () => {
+  it("renders About heading", () => {
+    render(<Aboutpage />);
+    expect(screen.getAllByRole("heading")[0].textContent).toEqual("About");
+  });
+
+  it("renders 'What is this' heading", () => {
+    render(<Aboutpage />);
+    expect(screen.getAllByRole("heading")[1].textContent).toEqual(
+      "What Is This?"
+    );
+  });
+});
