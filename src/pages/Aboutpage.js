ef2bd8bd847ebc1432ce135c77566472f0862923
ellismcdougald <ellismcdougald@icloud.com>
2022-08-09 14:39:28 -0600

    Adds content to Aboutpage

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -1,5 +1,41 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import styles from "../styles/Aboutpage.module.css";
 
 export default function Aboutpage() {
-  return <div data-testid='Aboutpage'></div>;
+  const [aboutText, setAboutText] = useState("");
+
+  async function getAboutText() {
+    const docRef = doc(db, "siteInfo", "About");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setAboutText(docSnap.data().bio);
+    } else {
+      console.log("error");
+    }
+  }
+
+  useEffect(() => {
+    getAboutText();
+  }, []);
+
+  return (
+    <div id={styles.container}>
+      <div id={styles.innerContainer}>
+        <div id={styles.menuDiv}>
+          <span id={styles.aboutHeader} role='heading' aria-level='1'>
+            <p>About</p>
+          </span>
+        </div>
+        <h1 id={styles.header}>What Is This?</h1>
+        <div id={styles.textDiv}>
+          <p id={styles.bioText} role='paragraph'>
+            {aboutText}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
