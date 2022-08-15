bc7ec473aa40cef3ca120680f8edea68c4fbef0f
ellismcdougald <ellismcdougald@icloud.com>
2022-08-15 12:01:38 -0600

    Switches to post prop and wraps render in BrowserRouter

--- src/components/PostPreview.test.js
+++ src/components/PostPreview.test.js
@@ -1,38 +1,60 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import PostPreview from "./PostPreview";
 
 describe("TopPostPreview component", () => {
-  it("renders correct post image", () => {
-    render(<PostPreview />);
-    const img = screen.getAllByRole("img")[0];
-    expect(img).toHaveAttribute(
-      "src",
-      "https://substackcdn.com/image/fetch/w_128,c_scale,f_png,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack.com%2Ficon%2FSubstackIcon%3Fheight%3D128%26fill%3D%2523DDDDDD%26stroke%3D%2523DDDDDD"
-    );
-    expect(img).toHaveAttribute("alt", "Default post image");
-  });
+  const samplePost = {
+    title: "Miro Heiskanen and aggressive/passive forechecks",
+    subtitle:
+      "Tampa Bay limits Miro Heiskanen's transition abilities bpy giving him more time to make a play, not less",
+    author: "draftlookhockey",
+    publishDate: "08/01/2022",
+    id: "1",
+  };
 
   it("renders correct post title", () => {
-    render(<PostPreview title='My Title' />);
-    expect(screen.getAllByRole("heading")[0].textContent).toMatch("My Title");
+    render(
+      <BrowserRouter>
+        <PostPreview post={samplePost} />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("heading")[0].textContent).toMatch(
+      samplePost.title
+    );
   });
 
   it("renders correct post subtitle", () => {
-    render(<PostPreview subtitle='My Subtitle' />);
+    render(
+      <BrowserRouter>
+        <PostPreview post={samplePost} />
+      </BrowserRouter>
+    );
     expect(screen.getAllByRole("heading")[1].textContent).toMatch(
-      "My Subtitle"
+      samplePost.subtitle
     );
   });
 
   it("renders correct post author", () => {
-    render(<PostPreview author='My Author' />);
-    expect(screen.getAllByRole("heading")[2].textContent).toMatch("My Author");
+    render(
+      <BrowserRouter>
+        <PostPreview post={samplePost} />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("heading")[2].textContent).toMatch(
+      samplePost.author.toUpperCase()
+    );
   });
 
   it("renders correct post date", () => {
-    render(<PostPreview date='2022-08-01' />);
-    expect(screen.getAllByRole("heading")[3].textContent).toMatch("2022-08-01");
+    render(
+      <BrowserRouter>
+        <PostPreview post={samplePost} />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("heading")[3].textContent).toMatch(
+      samplePost.publishDate
+    );
   });
 });
--- src/components/TopPostPreview.test.js
+++ src/components/TopPostPreview.test.js
@@ -1,33 +1,49 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
 import TopPostPreview from "./TopPostPreview";
 
 describe("TopPostPreview component", () => {
-  it("renders correct post image", () => {
-    render(<TopPostPreview />);
-    const img = screen.getAllByRole("img")[0];
-    expect(img).toHaveAttribute(
-      "src",
-      "https://substackcdn.com/image/fetch/w_128,c_scale,f_png,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack.com%2Ficon%2FSubstackIcon%3Fheight%3D128%26fill%3D%2523DDDDDD%26stroke%3D%2523DDDDDD"
-    );
-    expect(img).toHaveAttribute("alt", "Default post image");
-  });
+  const samplePost = {
+    title: "Miro Heiskanen and aggressive/passive forechecks",
+    subtitle:
+      "Tampa Bay limits Miro Heiskanen's transition abilities bpy giving him more time to make a play, not less",
+    author: "draftlookhockey",
+    id: "1",
+  };
 
   it("renders correct post title", () => {
-    render(<TopPostPreview title='My Title' />);
-    expect(screen.getAllByRole("heading")[0].textContent).toMatch("My Title");
+    render(
+      <BrowserRouter>
+        <TopPostPreview post={samplePost} />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("heading")[0].textContent).toMatch(
+      samplePost.title
+    );
   });
 
   it("renders correct post subtitle", () => {
-    render(<TopPostPreview subtitle='My Subtitle' />);
+    render(
+      <BrowserRouter>
+        <TopPostPreview post={samplePost} />
+      </BrowserRouter>
+    );
     expect(screen.getAllByRole("heading")[1].textContent).toMatch(
-      "My Subtitle"
+      samplePost.subtitle
     );
   });
 
   it("renders correct post author", () => {
-    render(<TopPostPreview author='My Author' />);
-    expect(screen.getAllByRole("heading")[2].textContent).toMatch("My Author");
+    render(
+      <BrowserRouter>
+        <TopPostPreview post={samplePost} />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole("heading")[2].textContent).toMatch(
+      samplePost.author
+    );
   });
 });
