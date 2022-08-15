5d2ecb4386847ea10bdd82033fd8e39d304f532c
ellismcdougald <ellismcdougald@icloud.com>
2022-08-15 12:04:04 -0600

    Deleted files

--- src/pages/Archivepage.test.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Archivepage from "./Archivepage";
-
-describe("Archivepage component", () => {});
--- src/pages/Homepage.test.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Homepage from "./Homepage";
-
-describe("Homepage component", () => {});
