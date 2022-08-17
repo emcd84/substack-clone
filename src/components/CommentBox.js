7410f1a2b2676f5d66d7327c1479ad1b497ebc49
ellismcdougald <ellismcdougald@icloud.com>
2022-08-16 18:59:34 -0600

    Adds parent and child options

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,19 @@
 import { React, useState } from "react";
 import styles from "../styles/CommentBox.module.css";
 
-export default function CommentBox() {
+export default function CommentBox(props) {
   const [commentText, setCommentText] = useState("");
 
   function trackCommentText(e) {
     setCommentText(e.target.value);
+  }
+
+  function textAreaId() {
+    if (props.type === "parent") {
+      return styles.parentInput;
+    } else if (props.type === "child") {
+      return styles.childInput;
+    }
   }
 
   return (
@@ -14,16 +22,34 @@ export default function CommentBox() {
         <img
           id={styles.img}
           src='https://substackcdn.com/image/fetch/w_80,h_80,c_fill,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack.com%2Fimg%2Favatars%2Forange.png'
+          alt='default'
         ></img>
         <textarea
           placeholder='Write a comment...'
-          id={styles.commentInput}
+          className={styles.commentInput}
+          id={textAreaId()}
           onChange={trackCommentText}
         ></textarea>
       </div>
-      {commentText.length > 0 && (
-        <div id={styles.buttonContainer}>
-          <button id={styles.postButton}>Post</button>
+      {props.type === "parent" && commentText.length > 0 && (
+        <div className={styles.buttonContainer}>
+          <button className={styles.postButton} id={styles.parentPostButton}>
+            Post
+          </button>
+        </div>
+      )}
+      {props.type === "child" && (
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.postButton}
+            id={styles.childCancelButton}
+            onClick={props.cancelCommentBox}
+          >
+            Cancel
+          </button>
+          <button className={styles.postButton} id={styles.childPostButton}>
+            Post
+          </button>
         </div>
       )}
     </div>
